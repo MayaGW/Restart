b03045d51b8f185fa9838324bc64c1188cf315d2
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-02-09 16:03:50 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Maya Ghamloush on 09/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
